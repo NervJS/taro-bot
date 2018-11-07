0e855ba3e1eb41523a4b079e886af3be2a054d34
yuche <i@yuche.me>
2018-11-07 15:03:00 +0800

    chore: 修改配置文件

--- src/config.ts
+++ src/config.ts
@@ -4,9 +4,23 @@ export const TO_BE_CLOSED_LABEL = 'to be closed'
 
 export const DAYS_UNTIL_CLOSE = 15
 
+const newIssueWelcomeComment = `欢迎提交 Issue~
+
+如果你提交的是 bug 报告，请务必遵循 [Issue 模板](https://github.com/NervJS/taro/blob/master/.github/ISSUE_TEMPLATE/bug_report.md)的规范，尽量用简洁的语言描述你的问题，最好能提供一个稳定简单的复现。🙏🙏🙏
+
+如果你的信息提供过于模糊或不足，或者已经其他 issue 已经存在相关内容，你的 issue 有可能会被关闭。`
+
+const newPRWelcomeComment = `欢迎提交 Pull Request~
+
+请检查您的代码符合 [JavaScript Standard Guide](https://github.com/standard/standard) 规范，您的提交信息也应当遵循 [Angular Style Commit Message Conventions](https://gist.github.com/stephenparish/9941e89d80e2bc58a153)。
+
+如果相关的包存在测试的话请务必确保所有测试用例都能通过，并添加 PR 内容相关的测试用例。
+
+Taro 非常感谢您对开源事业做出的贡献。👏👏👏`
+
 export const welcomeConfig: WelcomeConfig = {
-  newIssueWelcomeComment: '',
-  newPRWelcomeComment: ''
+  newIssueWelcomeComment,
+  newPRWelcomeComment
 }
 
 function buildComment (str: string) {
@@ -16,8 +30,7 @@ ${str}
 
 如果您在这 15 天中更新更多信息自动关闭的流程会自动取消，如有其他问题也可以发起新的 Issue。
 
-Good luck and happy coding~
-`
+Good luck and happy coding~`
 }
 
 export const markerConfigs: MarkerConfig[] = [
